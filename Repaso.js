584dfd7337639285f7c557319af3bd0f4095f1f7
OlozanoTamy <Olozanodeveloper@gmail.com>
2024-07-05 18:59:08 -0500

    First Commit

---
+++ Repaso.js
@@ -0,0 +1,32 @@
+//Introduccion al Bootcamp
+//Javascript en el Backend
+//Alto nivel, interpretado, tipado dinamico
+//Intertividad y funcionalidades dinamicas a las paginas web
+
+//Typescript
+//Es una extension de Javascript que introduce tipado estatico
+//Ofrece caracteristicas avanzadas en programacion orientada a objetos y ,
+//al ser transpilado puede ejecutarse en cualquier entorno que soporte javascript
+
+//Que es el Backend
+//Es la parte de un sistema o aplicacion que opera detras de escena, gestionando la logica de negocio , el acceso a la base de datos
+//la autenticacion, y otras operaciones cruciales que el usuario
+//no ve directamente
+
+//Repaso de Javascript
+//Prototipos - Clases - Intro a TS
+
+//Node JS
+//Internals -- Como funciona Node js
+//Servidores en node
+//Sistema de Archivos
+
+//Nest JS
+//Es un poco mas usado que Express , va un nivel de abstraccion que nestjs
+
+
+//Construccion de APIS
+
+//Despliegue - Protocolos - Testing - Documentacion
+
+//Bases de datos
