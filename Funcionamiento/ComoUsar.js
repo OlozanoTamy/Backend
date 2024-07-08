4461fbcd084a233814e7f329b30e03b301efc93d
OlozanoTamy <Olozanodeveloper@gmail.com>
2024-07-08 13:04:18 -0500

    First proyect

---
+++ Funcionamiento/ComoUsar.js
@@ -0,0 +1,50 @@
+//ECMSCRIPT
+//Estandar de JAVASCRIPT
+//Organizmo de Estandares
+//TC39
+//ECMASCRIPT != JAVASCRIPT
+//Javascript es una implementacion y extension o superset de ECMASCRIPT
+//Un poco de Historia
+//Brendan EICH crea JS en 10 Dias -> 1995
+//MOCHA -> LIVESRICPT -> JAVASCRIPT
+//SUN MICROSYSTEMS -> restriccion de no usar la sintaxis de objetos de Java
+//Los prototipos se encadenan
+//1996 - 1997 -> ECMA
+//1999 -> Se publica ECMASCRIPT 3 con importantes mejoras
+//1999-2008 -> se hace un desastre completo con la especificacion
+//Cada quien hace lo que quiere
+//2008 -> ECMASCRIPT 3.1 con pequeñas mejoras
+//2009 -> ECMASCRIPT 5
+//2015 -> ECMASCRIPT 6 -> MEJORAS SUSTANCIALES su nombre oficial es ECMAScript 2015
+//Despues se acordo publicar mejares incrementales no tan grandes cada año
+//Por que se dice que es Moderno ?
+//Call Stack
+
+
+//JavaScript Engines
+//Engine es un motor que ejecuta el archivo de JavaScript -> lenguaje interpretado
+//Compilador vs Interprete -> directamente ejecutado por el interprete
+//JS es un lenguaje compilado Just-in-Time -> no es una compilacion tradicional
+//V8
+//Just-in-Time
+//El codigo de javascript corre en varios hilos
+//JavaScript source code -> Parser ->  Abstract Syntax Tree -> Interpreter Ignition (Saca metricas de uso) ->
+//Compilador Turbo Fan -> Codigo maquina Optimizado -> Bytecode
+
+
+//Bytecode  -> Punto intermedio entre lenguaje de alto nivel y codigo maquina
+//Bytecode vive en v8
+//JAVA tiene JIT hace poco tiempo
+
+
+//Abstract Syntax tree
+//Arbol que dirige la ejecucion del programa -> se puede escribir de muchas formas
+//Despues compila
+
+
+//Transpilador -> ejecuta lenguante y lo escupe en otra forma
+
+//Runtime -> entorno de ejecucion  en este caso Node.JS
+//Proporciona APIS a V8 en las capacidades que no tiene respecto al manejo de
+//de el ordenador
+//Run time de windows chakra 
---
+++ ManosObra/Node.js
@@ -0,0 +1,38 @@
+//Rol de node en el backend
+//Setup inicial e instalacion
+//Crear primer proyecto
+//Conocer los modulos de node.js y su importancia
+//Crear un servidor
+
+
+
+//Rol de NODE.js en el Backend
+//Trancision sencilla al backend
+//Entorno de trabajo
+
+//Preparando entorno de trabajo
+
+//NVM -> mantener varias versiones de Node.js
+
+//Modulos de node.js
+//Herramientas para construir proyectos
+//Existen modulo Core que son nativos HTTP , CRYPTO , PATH , FS , PROCESS , CONSOLE
+//Ya estan disponibles dentro de node
+
+//Recomendacion del profesor -> nodejs.dev
+
+
+
+//Modules de terceros
+//Requieren Instalacion -> Extender funcianalidad
+//Ejemplos -> express , babel, react , prisma , lodash , @nestjs/core
+
+//Estos paquetes se pueden descargar desde npm (node package manager)
+//Es importante usar NpM
+
+//Tambien se pueden crear sus propios paquetes pueden ser privados o publicos
+//Codigo reusable
+
+
+//Manejador de paquetes de node
+//Es por defecto el manejador aunque existen alternativos pnpm y/o yarn
